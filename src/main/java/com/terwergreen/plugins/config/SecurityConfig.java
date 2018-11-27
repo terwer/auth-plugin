600723266d091b57a456557879137278adf8b4a4
Terwer <youweics@sina.com>
2018-11-27 17:14:38 +0800

    授权配置

---
+++ src/main/java/com/terwergreen/plugins/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.terwergreen.plugins.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * @Author Terwer
+ * @Date 2018/6/22 15:55
+ * @Version 1.0
+ * @Description 安全配置
+ **/
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
