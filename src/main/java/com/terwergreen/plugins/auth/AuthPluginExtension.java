09f1c957cbdd7f200043dc1f8ad79bacd52ab495
Terwer <youweics@sina.com>
2018-11-29 04:50:09 +0800

    授权开关

--- src/main/java/com/terwergreen/plugins/auth/AuthPluginExtension.java
+++ src/main/java/com/terwergreen/plugins/auth/AuthPluginExtension.java
@@ -1,16 +1,17 @@
 package com.terwergreen.plugins.auth;
 
 import com.terwergreen.plugins.PluginInterface;
-import com.terwergreen.plugins.auth.config.WebSecurityConfig;
+import com.terwergreen.plugins.auth.config.WebFluxSecurityConfig;
 import org.pf4j.Extension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebServerApplicationContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.reactive.function.server.RouterFunction;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Author Terwer
@@ -25,20 +26,27 @@ public class AuthPluginExtension implements PluginInterface {
 
     private void setApplicationContext(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
-        AnnotationConfigReactiveWebServerApplicationContext container = (AnnotationConfigReactiveWebServerApplicationContext) applicationContext;
-        logger.debug("AuthPlugin registerBean in container,applicationContext is:" + container);
-        container.registerBean(WebSecurityConfig.class);
-        logger.info("AuthPlugin registerBean " + WebSecurityConfig.class + " in container " + container);
+        // AnnotationConfigReactiveWebServerApplicationContext container = (AnnotationConfigReactiveWebServerApplicationContext) applicationContext;
+        // logger.debug("AuthPlugin registerBean in container,applicationContext is:" + container);
+        // container.registerBean(Object.class);
+        // logger.info("AuthPlugin registerBean " + Object.class + " in container " + container);
     }
 
     @Override
     public String identify() {
-        return "AuthPlugin identify in plugin";
+        return "AuthPlugin";
     }
 
     @Override
     public List<?> reactiveRoutes() {
         return new ArrayList<RouterFunction<?>>() {{
         }};
+    }
+
+    @Override
+    public Map data() {
+        Map dataMap = new HashMap();
+        dataMap.put("securityOn", WebFluxSecurityConfig.SECURITY_ON);
+        return dataMap;
     }
 }
---
+++ src/main/java/com/terwergreen/plugins/auth/config/WebFluxSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.terwergreen.plugins.auth.config;
+
+/**
+ * @Author Terwer
+ * @Date 2018/6/22 15:55
+ * @Version 1.0
+ * @Description 安全授权配置
+ **/
+public class WebFluxSecurityConfig {
+    /**
+     * 是否开启安全授权
+     */
+    public static final int SECURITY_ON = 1;
+}
+
+
