2eb48e48e34936d7d714e7a601e7ef938c61e62b
Terwer <youweics@sina.com>
2018-11-29 12:29:16 +0800

    配置管理后台地址

--- src/main/java/com/terwergreen/plugins/auth/AuthPluginExtension.java
+++ src/main/java/com/terwergreen/plugins/auth/AuthPluginExtension.java
@@ -1,10 +1,12 @@
 package com.terwergreen.plugins.auth;
 
+import com.terwergreen.core.CommonService;
 import com.terwergreen.plugins.PluginInterface;
 import com.terwergreen.plugins.auth.config.WebFluxSecurityConfig;
 import org.pf4j.Extension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.reactive.function.server.RouterFunction;
 
@@ -23,6 +25,9 @@ import java.util.Map;
 public class AuthPluginExtension implements PluginInterface {
     private static final Logger logger = LoggerFactory.getLogger(AuthPluginExtension.class);
     private ApplicationContext applicationContext;
+
+    @Autowired
+    private CommonService commonService;
 
     private void setApplicationContext(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
@@ -48,6 +53,9 @@ public class AuthPluginExtension implements PluginInterface {
         Map dataMap = new HashMap();
         dataMap.put("securityOn", WebFluxSecurityConfig.SECURITY_ON);
         dataMap.put("loginPath", WebFluxSecurityConfig.LOGIN_PATH);
+        // 查询后台地址
+        String adminPath = (String) commonService.getSiteConfig("adminPath");
+        dataMap.put("adminPath", adminPath);
         return dataMap;
     }
 }
