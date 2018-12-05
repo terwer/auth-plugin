e18c2d1876cab045252a1c6adf06d092c78cfba8
Terwer <youweics@sina.com>
2018-12-05 18:04:25 +0800

    登陆流程调通

--- src/main/java/com/terwergreen/plugins/auth/front/AuthController.java
+++ src/main/java/com/terwergreen/plugins/auth/front/AuthController.java
@@ -1,10 +1,11 @@
 package com.terwergreen.plugins.auth.front;
 
-import com.terwergreen.plugins.auth.service.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.google.common.net.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 控制器
@@ -16,13 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("auth")
 public class AuthController {
-    @Autowired
-    private AuthService authService;
-
     @RequestMapping(value = "login", produces = {"text/plain;charset=utf-8"})
-    public String auth(Model model) {
-        String systemInfo = authService.getSystemInfo();
-        model.addAttribute("systemInfo", systemInfo);
+    public String auth(Model model, HttpServletRequest request) {
+        String referrer = request.getHeader(HttpHeaders.REFERER);
+        model.addAttribute("referer", referrer);
         return "login";
     }
 }
