ddd38f26e1e85010d19f849ad148fddde669cfce
terwer <youweics@sina.com>
2018-12-21 01:04:23 +0800

    兼容eclipse配置

--- src/main/java/com/terwergreen/plugins/auth/AuthPlugin.java
+++ src/main/java/com/terwergreen/plugins/auth/AuthPlugin.java
@@ -1,34 +1,34 @@
 package com.terwergreen.plugins.auth;
 
-import com.terwergreen.plugins.BugucmsPlugin;
 import org.pf4j.PluginException;
 import org.pf4j.PluginWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.terwergreen.plugins.BugucmsPlugin;
+
 /**
  * Hello World
  *
  * @author Terwer
- * @version 1.0
- * 2018/11/27 16:03
+ * @version 1.0 2018/11/27 16:03
  **/
 public class AuthPlugin extends BugucmsPlugin {
-    private static final Logger logger = LoggerFactory.getLogger(AuthPlugin.class);
+	private static final Logger logger = LoggerFactory.getLogger(AuthPlugin.class);
 
-    public AuthPlugin(PluginWrapper wrapper) {
-        super(wrapper);
-    }
+	public AuthPlugin(PluginWrapper wrapper) {
+		super(wrapper);
+	}
 
-    @Override
-    public void start() throws PluginException {
-        super.start();
-        logger.info("AuthPlugin started");
-    }
+	@Override
+	public void start() throws PluginException {
+		super.start();
+		logger.info("AuthPlugin started");
+	}
 
-    @Override
-    public void stop() {
-        super.stop();
-        logger.info("AuthPlugin stoped");
-    }
+	@Override
+	public void stop() {
+		super.stop();
+		logger.info("AuthPlugin stoped");
+	}
 }
--- src/main/java/com/terwergreen/plugins/auth/front/AuthController.java
+++ src/main/java/com/terwergreen/plugins/auth/front/AuthController.java
@@ -1,23 +1,22 @@
 package com.terwergreen.plugins.auth.front;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * 控制器
  *
  * @author Terwer
- * @version 1.0
- * 2018/11/29 1:23
+ * @version 1.0 2018/11/29 1:23
  **/
 @Controller
 @RequestMapping("auth")
 public class AuthController {
-    @RequestMapping(value = "login", produces = {"text/plain;charset=utf-8"})
-    public String auth(Model model, HttpServletRequest request) {
-        return "login";
-    }
+	@RequestMapping(value = "login", produces = { "text/plain;charset=utf-8" })
+	public String auth(Model model, HttpServletRequest request) {
+		return "auth/login";
+	}
 }
